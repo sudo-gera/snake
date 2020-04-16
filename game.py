2ee7556066816338d64fefa3ebe0943c5e15a410
sudo-gera <gera.tatarinov.999@mail.com>
2020-04-16 10:46:49 +0300

    bug

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import os,time,random,sys
 term=os.get_terminal_size
 os.system('>key.a')
-print('\n'*(term()[1]-2))
+print('\n'*(term()[1]-2),)
 ls=0
 q=5
 a=5
@@ -28,47 +28,69 @@ aver=len(sn)/1.0
 llen=1
 col=0
 ons=[]
+spc=0
+lpl=[]
+loul=''
 while 1:
  time.sleep(1/len(sys.argv[1]))
  if col==1:
-  print('\x1b['+str(random.choice(fc))+';'+str(random.choice(bc))+'m')
+  print('\x1b['+str(random.choice(fc))+';'+str(random.choice(bc))+'m',)
  if col==2:
-  print('\x1b[5;'+str(random.choice(fc))+';'+str(random.choice(bc))+'m')
- print('\x1b[H')
+  print('\x1b[5;'+str(random.choice(fc))+';'+str(random.choice(bc))+'m',)
+ print('\x1b[H',end='')
  ns=sn[:]
  ns+=[[ea,eq]]
  ns+=bd
  pns=[w for w in ns if w not in ons]
  mns=[w for w in ons if w not in ns]
- print('\x1b[33m')
+ print('\x1b[33m',)
  for w in pns:
   if w[1]%2:
    if [w[0],w[1]-1] in ns:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H█')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H█',)
    else:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▄')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▄',)
   else:
    if [w[0],w[1]+1] in ns:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H█')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H█',)
    else:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▀')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▀',)
  for w in mns:
   if w[1]%2:
    if [w[0],w[1]-1] in ns:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▀')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▀',)
    else:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H ')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H ',)
   else:
    if [w[0],w[1]+1] in ns:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▄')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H▄',)
    else:
-    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H ')
+    print('\x1b['+str(w[1]//2+1)+';'+str(w[0]+1)+'H ',)
  ons=ns[:]
- print('\x1b[35m')
- print('\x1b['+str(term()[1]-3)+';0H'+'  '*term()[0])
- print('\x1b['+str(term()[1]-2)+';0H'+'now:',len(sn),'max:',ma,'aver:',aver)
- print('\x1b['+str(term()[1]-3)+';0H'+'█'*(bdl*term()[0]//bdw)+int(bool(max(bdl,0)))*chr(9615-max(bdl,0)*term()[0]*8//bdw%8))
- print('\x1b['+str(term()[1]-4)+';0H'+'═'*term()[0])
+ print('\x1b[35m',)
+ oul='█'*(bdl*term()[0]//bdw)+int(bool(max(bdl,0)))*chr(9615-max(bdl,0)*term()[0]*8//bdw%8)
+ oub=oue=0
+ if len(loul)!=len(oul):
+  print('\x1b['+str(term()[1]-3)+';0H'+'  '*term()[0],)
+  print('\x1b['+str(term()[1]-3)+';0H'+oul,)
+  loul=oul
+ else:
+  oub=0
+  while oub<len(oul) and oul[oub]==loul[oub]:
+   oub+=1
+  oue=len(oul)
+  while oue>oub and oul[oue-1]==loul[oue-1]:
+   oue-=1
+  print('\x1b['+str(term()[1]-3)+';'+str(oub)+'H'+oul[oub:oue],end='')
+# print('\x1b['+str(term()[1]-2)+';0H'+'now:',len(sn),'max:',ma,'aver:',averwnd='')
+ if lpl!=[len(sn),ma] or oue!=oub:
+  print('\x1b['+str(term()[1]-2)+';0H'+'now:',len(sn),'max:',ma,)
+  lpl=[len(sn),ma]
+ if spc==0:
+  print('\x1b['+str(term()[1]-4)+';0H'+'═'*term()[0],)
+  spc=128
+ print('\x1b[0;0H',end='')
+ spc-=1
  if bdc==bdm:
   bdq=random.randint(1,term()[1]*2-12)
   bda=random.randint(1,term()[0]-2)
@@ -139,8 +161,8 @@ while 1:
     lf=f
    a%=term()[0]
   elif f=='p':
-   print('\x1b[0m')
-   print('\x1b[0;0H'+' '*term()[0]*(term()[1]-2))
+   print('\x1b[0m',)
+   print('\x1b[0;0H'+' '*term()[0]*(term()[1]-2),)
    print('\x1b[0;0H',end='')
    exit()
  if [a,q] in sn:
